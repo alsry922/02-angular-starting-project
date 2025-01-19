54c5e7066571a4b047b193931665e5af0c334310
alsry922 <alsry922@gmail.com>
2025-01-20 07:52:38 +0900

    [15]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from "./header/header.component";
 
+// Angular의 Component 데코레이터는 클래스의 메타데이터를 제공한다.
+// 컴포넌트 데코레이터는 클래스를 Angular 컴포넌트로 변환하여 처리되도록 한다.
+// index.html 보면 app-root 태그가 있음.
+// 셀릭터를 정의함으로써 Angular가 html에서 어떤 요소를 찾아야 하는지 알려주고 해당 컴포넌트와 템플릿으로 대체한다.
+// styleUrl 속성에 정의한 css 파일은 기본적으로 해당 컴포넌트에만 적용되며, 여러 적용범위 옵션도 존재한다.
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
+// 웹사이트가 로드될 때 브라우저는 index.html을 가져와서 보여줌.
+// index.html에는 스크립트 파일을 가져오기 위한 어떤 코드도 없음
+// 하지만 브라우저 도구를 열면 polifill.js, main.js 파일이 script 태그로 import 하고있음.
+// ng serve를 통해 이 처리가 자동으로 이루어짐
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
